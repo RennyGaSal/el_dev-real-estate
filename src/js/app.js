ef3f7df3ec764aad66e891ac237463d1b3b3a3be
Renny GaSal <rennygs.dev@gmail.com>
2025-09-18 17:21:40 -0500

    feat(navigation): add mobile menu functionality and improve nav visibility

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    eventListeners();
+});
+
+function eventListeners() {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.addEventListener('click', responsiveNav);
+}
+
+function responsiveNav() {
+    const nav = document.querySelector('.nav');
+    nav.classList.toggle('show');
+}
