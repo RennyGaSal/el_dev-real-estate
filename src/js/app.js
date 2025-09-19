792578f5a32b7d92289ffac33a6fde63766f4ea2
Renny GaSal <rennygs.dev@gmail.com>
2025-09-18 21:46:26 -0500

    feat(nav): implement mobile menu and dark mode functionality across multiple pages

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
-    eventListeners();
+    responsiveNav();
+
+    darkMode();
 });
 
-function eventListeners() {
-    const mobileMenu = document.querySelector('.mobile-menu');
-    mobileMenu.addEventListener('click', responsiveNav);
+function darkMode() {
+    const darkModeButton = document.querySelector('.dark-mode-button');
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+    
+    if (prefersDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+        darkModeButton.classList.add('active');
+    }
+
+    darkModeButton.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode');
+        darkModeButton.classList.toggle('active');
+    });
+
+    prefersDarkMode.addEventListener('change', function() {
+        if (prefersDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+            darkModeButton.classList.add('active');
+        } else {
+            document.body.classList.remove('dark-mode');
+            darkModeButton.classList.remove('active');
+        }
+    });
 }
 
 function responsiveNav() {
-    const nav = document.querySelector('.nav');
-    nav.classList.toggle('show');
+    const mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.addEventListener('click', function() {
+        const nav = document.querySelector('.nav');
+        nav.classList.toggle('show');
+        mobileMenu.classList.toggle('active');
+    });
 }
